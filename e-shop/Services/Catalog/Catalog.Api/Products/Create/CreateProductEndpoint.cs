30c8c10b3f9b4379eb7e7269b9a2d1d0dac7863b
leonardo-terra <leo.terra.melo@gmail.com>
2025-05-21 23:15:00 -0300

    Implements create product endpoint

--- e-shop/Services/Catalog/Catalog.Api/Products/Create/CreateProductEndpoint.cs
+++ e-shop/Services/Catalog/Catalog.Api/Products/Create/CreateProductEndpoint.cs
@@ -1,6 +1,58 @@
-﻿namespace Catalog.Api.Products.Create
+﻿using Carter;
+using Catalog.Api.Models;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Products.Create
 {
-    public class CreateProductEndpoint
+    public record class CreateProductRequest(string Name, string Description, string ImageFile, decimal Price, List<Category> Categories);
+    public record class CreateProductResponse(Guid Id);
+    internal class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products",
+                 async (CreateProductRequest request, ISender sender, CancellationToken cancellationToken) =>
+              {
+                  var command = request.Adapt<CreateProductCommand>();
+                  var result = await sender.Send(command, cancellationToken);
+
+                  var response = result.Adapt<CreateProductResponse>();
+                  return Results.Created($"/products/{response.Id}", response);
+
+              })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create a new product");
+        }
+    }
+    [ApiController]
+    [Route("products")]
+    public class CreateProductController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public CreateProductController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CreateProductResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
+        [ApiExplorerSettings(GroupName = "Create Product")]
+        [ActionName("CreateProduct")]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductRequest request, CancellationToken cancellationToken)
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await _sender.Send(command, cancellationToken);
+
+            var response = result.Adapt<CreateProductResponse>();
+            return Created($"/products/{response.Id}", response);
+        }
     }
 }
--- e-shop/Services/Catalog/Catalog.Api/Program.cs
+++ e-shop/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,15 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR((config) =>
+{
+    config.RegisterServicesFromAssemblyContaining<Program>();
+});
+ 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
