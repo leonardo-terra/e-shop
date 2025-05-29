7a936bff505f94b610680fe1f8bcd709ba0cf59e
leonardo <leo.terra.melo@gmail.com>
2025-05-29 19:08:32 -0300

    Implements get all products route

---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Get/GetProductsEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Catalog.Api.Models;
+using Catalog.Api.Products.Get;
+using Mapster;
+using MediatR;
+
+public record GetProductsRequest();
+
+public record GetProductsResponse(List<Product> ProductsList);
+
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products",
+            async (ISender sender) =>
+            {
+                var query = new GetProductsQuery();
+                var result = await sender.Send(query);
+                result.Adapt<GetProductsResponse>();
+                return Results.Ok(result);
+            })
+        .WithName("GetProducts")
+        .Produces<List<Product>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Get/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Models;
+using Marten;
+using MediatR;
+
+namespace Catalog.Api.Products.Get
+{
+    public record GetProductsQuery() : IRequest<GetProductsResult>;
+
+    public record GetProductsResult(List<Product> productList);
+    internal class GetProductsHandler(IDocumentSession dbSession) : IRequestHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
