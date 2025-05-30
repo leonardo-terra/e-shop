5edbd7d919948b477aad6e959c3ec6b4f9d8cff3
leonardo <leo.terra.melo@gmail.com>
2025-05-30 08:27:52 -0300

    Implement getproduct by ID

---
+++ e-shop/Services/Catalog/Catalog.Api/Products/GetById/GetProductByIdEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Carter;
+using Catalog.Api.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.GetById
+{
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id:guid}",
+                async (Guid id, ISender sender) =>
+                {
+                    try
+                    {
+                        var query = new GetProductByIdQuery(id);
+                        var result = await sender.Send(query);
+                        return Results.Ok(result.Adapt<GetProductByIdResponse>());
+                    }
+                    catch (KeyNotFoundException ex)
+                    {
+                        return Results.BadRequest(ex.Message);    
+                    }
+                })
+        .WithName("GetProductById")
+        .Produces<List<Product>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Returns product by Id")
+        .WithDescription("Get Product");
+        }
+    }
+}
---
+++ e-shop/Services/Catalog/Catalog.Api/Products/GetById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.Api.Models;
+using Marten;
+using MediatR;
+
+namespace Catalog.Api.Products.GetById
+{
+    public record GetProductByIdQuery(Guid Id) : IRequest<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdHandler(IDocumentSession session) : IRequestHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await session.LoadAsync<Product>(request.Id);
+
+            if (result is null)
+                throw new KeyNotFoundException($"Product with ID {request.Id} not found.");
+
+            return new GetProductByIdResult(result);
+        }
+    }
+}
