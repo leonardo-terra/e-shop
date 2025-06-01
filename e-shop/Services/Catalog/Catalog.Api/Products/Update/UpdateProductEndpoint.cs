22bf691bbbccf6734b75b6ea0b9377b4da0299da
leonardo <leo.terra.melo@gmail.com>
2025-06-01 17:59:39 -0300

    implement update product

---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Update/UpdateProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using Catalog.Api.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.Update
+{
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public record UpdateProductResponse(Product Product);
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products/update",
+                async (UpdateProductCommand command, ISender sender) =>
+                {
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<UpdateProductResponse>();
+
+                    return Results.Ok(response);
+                });
+        }
+    }
+}
---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Update/UpdateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Models;
+using Marten;
+using MediatR;
+
+namespace Catalog.Api.Products.Update
+{
+    public record UpdateProductCommand(Product Product) : IRequest<UpdateProductResponse>;
+    public record UpdateProductResponse(Product Product);
+    internal class UpdateProductHandler(IDocumentSession session) : IRequestHandler<UpdateProductCommand, UpdateProductResponse>
+    {
+        public async Task<UpdateProductResponse> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            session.Update(request.Product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResponse(request.Product);
+        }
+    }
+}
