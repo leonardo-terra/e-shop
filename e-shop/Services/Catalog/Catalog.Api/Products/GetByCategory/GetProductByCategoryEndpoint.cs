c7673543dc9da548e4b9b1efbf883ef30fc859be
leonardo <leo.terra.melo@gmail.com>
2025-06-01 17:09:13 -0300

    Implement get product by category

---
+++ e-shop/Services/Catalog/Catalog.Api/Products/GetByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using Carter;
+using Catalog.Api.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.GetByCategory
+{
+    public class GetProductByCategoryEndpoint : ICarterModule
+    {
+        public record GetProductByCategoryResult(IEnumerable<Product> Products);
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender mediator) =>
+            {
+                try
+                {
+                    var query = new GetProductByCategoryQuery(category);
+                    var result = await mediator.Send(query);
+                    var response = result.Adapt<GetProductByCategoryResult>();
+
+                    return Results.Ok(response);
+                }
+                catch (KeyNotFoundException ex)
+                {
+                    return Results.BadRequest(ex.Message);
+                }
+            })
+
+        .WithName("GetProductByCategory")
+        .Produces<List<Product>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Returns products by Category")
+        .WithDescription("Get Product by Category");
+        }
+    }
+}
---
+++ e-shop/Services/Catalog/Catalog.Api/Products/GetByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Api.Models;
+using Marten;
+using MediatR;
+
+namespace Catalog.Api.Products.GetByCategory
+{
+    public record GetProductByCategoryQuery(string Categories) : IRequest<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+
+    internal class GetProductByCategoryHandler(IDocumentSession session) : IRequestHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+            {
+
+            var result = await session.Query<Product>()
+                .Where(p => p.Categories.Any(c => c.Equals(request.Categories, StringComparison.OrdinalIgnoreCase)))
+                .ToListAsync(cancellationToken);
+
+            if (result is null || !result.Any())
+                throw new KeyNotFoundException($"No products found in category {request.Categories}.");
+
+            return new GetProductByCategoryResult(result);
+        }
+    }
+}
