79df0efcda44f5838bcc8f1edf2755d71239e5b8
leonardo <leo.terra.melo@gmail.com>
2025-06-01 18:14:42 -0300

    Implement Delete

---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Delete/DeleteProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.Delete
+{
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        private record DeleteProductResponse(Guid Id);
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id:guid}",
+                async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ e-shop/Services/Catalog/Catalog.Api/Products/Delete/DeleteProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Api.Models;
+using Marten;
+using MediatR;
+
+namespace Catalog.Api.Products.Delete
+{
+    public record DeleteProductCommand(Guid Id) : IRequest<DeleteProductResponse>;
+    public record DeleteProductResponse(Guid Id);
+
+    public class DeleteProductHandler(IDocumentSession session) : IRequestHandler<DeleteProductCommand, DeleteProductResponse>
+    {
+        public async Task<DeleteProductResponse> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            session.Delete<Product>(request.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResponse(request.Id);
+        }
+    }
+}
